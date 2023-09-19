7c958a0f4285c9ac263f15383234ca025801de17
nurcinozer <nrcinozer@gmail.com>
2023-09-19 15:25:08 +0300

    feat: added autocomplete api

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,15 @@
 'use client';
-
+import {
+	GIPHY_API_BASE_URL,
+	GIPHY_API_LIMIT,
+	GIPHY_API_SEARCH_SUGGESTIONS_ENDPOINT,
+} from '@/constants';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export const SearchBar = () => {
 	const [search, setSearch] = useState('');
+	const [suggestions, setSuggestions] = useState<string[]>([]);
 
 	const router = useRouter();
 
@@ -12,6 +17,21 @@ export const SearchBar = () => {
 		e.preventDefault();
 		setSearch('');
 		router.push(`/search/${search}`);
+	};
+
+	const handleSuggestions = async (
+		e: React.ChangeEvent<HTMLInputElement>,
+	) => {
+		e.preventDefault();
+		setSearch(e.target.value);
+
+		const res = await fetch(
+			`${GIPHY_API_BASE_URL}${GIPHY_API_SEARCH_SUGGESTIONS_ENDPOINT}?api_key=${process.env.NEXT_PUBLIC_GIPHY_API_KEY}&q=${e.target.value}&limit=${GIPHY_API_LIMIT}`,
+		);
+		const data = await res.json();
+		setSuggestions(
+			data.data.map((suggestion: { name: string }) => suggestion.name),
+		);
 	};
 
 	return (
@@ -22,12 +42,27 @@ export const SearchBar = () => {
 				className="border border-gray-400 rounded-lg py-2 px-4 text-black"
 				type="text"
 				value={search}
-				onChange={e => setSearch(e.target.value)}
+				onChange={e => handleSuggestions(e)}
 				placeholder="Search for a gif"
 			/>
 			<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
 				Search
 			</button>
+			{suggestions.length > 0 && (
+				<div className="absolute flex flex-col gap-2 bg-white border border-gray-400 rounded-lg py-2 px-4 text-black">
+					{suggestions.map(suggestion => (
+						<button
+							key={suggestion}
+							className="hover:bg-gray-200"
+							onClick={() => {
+								setSearch(suggestion);
+								setSuggestions([]);
+							}}>
+							{suggestion}
+						</button>
+					))}
+				</div>
+			)}
 		</form>
 	);
 };
--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ export const GIPHY_API_BASE_URL = 'https://api.giphy.com/v1/gifs';
 export const GIPHY_API_TRENDING_ENDPOINT = '/trending';
 export const GIPHY_API_LIMIT = 5;
 export const GIPHY_API_SEARCH_ENDPOINT = '/search';
+export const GIPHY_API_SEARCH_SUGGESTIONS_ENDPOINT = '/search/tags';
